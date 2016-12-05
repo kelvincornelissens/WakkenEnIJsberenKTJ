5706d3eb4270b8a4399a09c9708441b80974815d
kelvincornelissens <kelvincornelissens@hotmail.com>
2016-12-05 12:33:33 +0100

    Activity buttons navigatie

--- WakkenEnIJsberenKTJ/app/src/main/java/com/kelvin/wakkenenijsberenktj/GameGuide.java
+++ WakkenEnIJsberenKTJ/app/src/main/java/com/kelvin/wakkenenijsberenktj/GameGuide.java
@@ -1,13 +1,27 @@
 package com.kelvin.wakkenenijsberenktj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameGuide extends AppCompatActivity {
+    Button guideBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_guide);
+
+        guideBackButton = (Button)findViewById(R.id.guidebackBtn);
+
+        guideBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- WakkenEnIJsberenKTJ/app/src/main/java/com/kelvin/wakkenenijsberenktj/MainActivity.java
+++ WakkenEnIJsberenKTJ/app/src/main/java/com/kelvin/wakkenenijsberenktj/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kelvin.wakkenenijsberenktj;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,11 +10,16 @@ public class MainActivity extends AppCompatActivity {
 
     Music music = new Music();
     int state = 0;
+    Button playBtn;
+    Button guideBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button songButton = (Button)findViewById(R.id.songButton);
+        playBtn = (Button)findViewById(R.id.playBtn);
+        guideBtn = (Button)findViewById(R.id.guideBtn);
 
         music.initMediaPlayer(MainActivity.this, R.raw.seinfield);
         music.play();
@@ -34,6 +40,23 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), LevelSelect.class);
+                startActivity(i);
+            }
+        });
+
+        guideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), GameGuide.class);
+                startActivity(i);
+            }
+        });
+
     }
 
     @Override
